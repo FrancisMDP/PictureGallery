f0f51cada3c81ebe38c91de5a44b1437c02a2b88
Francisco <fomldspo@gmail.com>
2016-11-16 12:07:55 +0000

    Layout_fragment fixes

---
+++ app/src/main/java/com/ipbeja/pdm2/picturegallery/PicList.java
@@ -0,0 +1,24 @@
+package com.ipbeja.pdm2.picturegallery;
+
+/**
+ * Created by Francisco on 16/11/2016.
+ */
+
+public class PicList {
+    static String[] picName = {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"
+    };
+    static String[] picContent = {
+            "161",
+            "252",
+            "343",
+            "434",
+            "525",
+            "616"
+    };
+}
