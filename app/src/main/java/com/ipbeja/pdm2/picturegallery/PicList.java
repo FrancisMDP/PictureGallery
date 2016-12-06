5157cc22244327eb1552b0661ca470abe5d4b459
Francisco <fomldspo@gmail.com>
2016-12-06 11:01:53 +0000

    Many things

--- app/src/main/java/com/ipbeja/pdm2/picturegallery/PicList.java
+++ app/src/main/java/com/ipbeja/pdm2/picturegallery/PicList.java
@@ -1,11 +1,24 @@
 package com.ipbeja.pdm2.picturegallery;
 
+import android.os.Environment;
+
+import java.io.File;
+
 /**
  * Created by Francisco on 16/11/2016.
  */
 
 public class PicList {
+    String path = Environment.DIRECTORY_PICTURES;
+    String path2 = Environment.DIRECTORY_DCIM;
+    File f = new File(path);
+    File f2 = new File(path2);
+    File[] files = f.listFiles(new ImageFileFilter());
+    File[] files2 = f2.listFiles(new ImageFileFilter());
+
+
     static String[] picName = {
+            "Nova imagem...",
             "1",
             "2",
             "3",
@@ -14,6 +27,7 @@ public class PicList {
             "6"
     };
     static String[] picContent = {
+            "",
             "161",
             "252",
             "343",
